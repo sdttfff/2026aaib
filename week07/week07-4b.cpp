7e353e2291601727328d7f486388b3775533cedf
sdttfff <14751203@me.mcu.edu.tw>
2026-04-08 11:59:33 +0800

    week07

---
+++ week07/week07-4b.cpp
@@ -0,0 +1,9 @@
+// week07-4b.cpp SOIT107_Base_007
+// Method 2: char c
+#include <stdio.h>
+int main()
+{
+    char c1, c2, c3, c4, c5;
+    scanf("%c%c%c%c%c", &c1, &c2, &c3, &c4, &c5);
+    printf("%c   %c   %c   %c   %c", c1, c2, c3, c4, c5);
+}
---
+++ week07/week07-5b.cpp
@@ -0,0 +1,9 @@
+//week07-5b.cpp SOIT_BASE_009
+#include <stdio.h>
+int main()
+{
+     int a;
+     scanf("%d",&a);
+     if(a>2)printf("%d",a-2);
+     else printf("%d",2-a);
+}
